182d381efb48e7f02a165e23826e8ba87d9786c9
Abdllah <mcabdo25@gmail.com>
2021-02-02 03:57:33 +0100

    jhdhdhd

--- theproject/settings.py
+++ theproject/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'theproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'readosedb2',
+        'USER': 'postgres',
+        'PASSWORD': 'A1Z2E3R4T5',
+        'PORT': '5432',
+        'HOST': 'readosedb2.cjoeqo2vpykp.eu-west-3.rds.amazonaws.com'
     }
 }
 
